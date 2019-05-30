5ab0b6a6b2e3f6c9071bdbeb3b9d8ca3dd1cb94f
kindtanton <kindtchiva@gmail.com>
2019-05-30 16:06:47 +0200

    betere manier van questions tonen

--- client/src/components/user/Questions.jsx
+++ client/src/components/user/Questions.jsx
@@ -1,62 +1,29 @@
 import React from "react";
 import { inject, observer, PropTypes } from "mobx-react";
+import Question from './Question';
 //import styles from "./Answers.module.css";
+
 
 const Questions = ({ questionStore }) => {
     const { questions } = questionStore;
   return (
-    <>
-      <div>
-        <h2>questions</h2>
-        <form method="post">
-          <input
-            type="text"
-            name="clientToken"
-            id="clientToken"
-            value="value"
-          />
-          <label for="clientToken">Client Token</label>
-            {questions.map(question=>{
-              console.log(question);
-            })}
-          <div number="3">
-            <h3>text</h3>
-            <div>
-              <div>
-                <input
-                  type="radio"
-                  name="question"
-                  id="optionsRadios1"
-                  value="true"
-                />
-                <label for="optionsRadios1">Yes</label>
-              </div>
-              <div>
-                <input
-                  type="radio"
-                  name="question"
-                  id="optionsRadios2"
-                  value="false"
-                />
-                <label for="optionsRadios2">No</label>
-              </div>
-              <input type="text" name="question" id="optionsText2" />
-              <label for="optionsText2">Input</label>
-            </div>
-            <div>
-              <div>1</div>
-              <div>2</div>
-              <div>3</div>
-              <div>4</div>
-              <div>5</div>
-            </div>
-          </div>
+      <>
           <div>
-            <input type="submit" name="action" value="Submit" />
+              <h2>questions</h2>
+              <form method="post">
+                  <input
+                      type="text"
+                      name="clientToken"
+                      id="clientToken"
+                      value="value"
+                  />
+                  <label for="clientToken">Client Token</label>
+                  <ul>
+                      {questions.map(question => <Question question={question}/>)}
+                  </ul>
+              </form>
           </div>
-        </form>
-      </div>
-    </>
+      </>
   );
 };
 
