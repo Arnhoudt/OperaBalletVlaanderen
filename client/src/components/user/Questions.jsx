d0cee67d6c44f9aedd8146a5ab61917788c12c5d
Fredcyp <frederikcypers1705@gmail.com>
2019-05-30 20:23:05 +0200

    little changes

--- client/src/components/user/Questions.jsx
+++ client/src/components/user/Questions.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import { inject, observer, PropTypes } from "mobx-react";
-import Question from './Question';
+import Question from "./Question";
 //import styles from "./Answers.module.css";
 
-
 const Questions = ({ questionStore }) => {
-    const { questions } = questionStore;
+  const { questions } = questionStore;
+
   return (
-      <>
-          <div>
-              <h2>questions</h2>
-              <form method="post">
-                  <input
-                      type="text"
-                      name="clientToken"
-                      id="clientToken"
-                      value="value"
-                  />
-                  <label for="clientToken">Client Token</label>
-                  <ul>
-                      {questions.map(question => <Question question={question}/>)}
-                  </ul>
-              </form>
-          </div>
-      </>
+    <>
+      <div>
+        <h2>questions</h2>
+        <form method="post">
+          <input
+            type="text"
+            name="clientToken"
+            id="clientToken"
+            value="value"
+          />
+          <label for="clientToken">Client Token</label>
+          <ul>
+            {questions.map(question => (
+              <Question key={question._id} question={question} />
+            ))}
+          </ul>
+        </form>
+      </div>
+    </>
   );
 };
 
