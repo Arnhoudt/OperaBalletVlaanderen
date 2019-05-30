eb36cda11f2973ec021bf87180ee160693a1af49
kindtanton <kindtchiva@gmail.com>
2019-05-30 11:36:49 +0200

    kleine aanpassingne

--- server/app/routes/question.routes.js
+++ server/app/routes/question.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const {checkToken} = require('../middleware');
   const controller = require('../controllers/question.controller.js');
   app.post('/api/questions/add', checkToken, controller.create);
-  app.get('/api/questions', checkToken, controller.findAll);
+  app.get('/api/questions', controller.findAll);
   app.put('/api/questions/:questionId', checkToken, controller.update);
   app.delete('/api/questions/:questionId', checkToken, controller.delete);
 };
