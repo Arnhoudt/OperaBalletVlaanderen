3d6dac5367a8c11d747096c0ed6e9c70248337ab
Fredcyp <frederikcypers1705@gmail.com>
2019-05-30 13:35:29 +0200

    home updates

--- client/src/components/user/Questions.jsx
+++ client/src/components/user/Questions.jsx
@@ -3,110 +3,55 @@ import { inject, observer, PropTypes } from "mobx-react";
 //import styles from "./Answers.module.css";
 
 const Questions = ({ questionStore }) => {
-  const { questions } = questionStore;
-
-  const questionName = React.createRef();
-  const questionParam1 = React.createRef();
-  const questionParam2 = React.createRef();
-  const questionParam3 = React.createRef();
-  const questionParam4 = React.createRef();
-  const questionParam5 = React.createRef();
-  let questionAnswerType = `BOOL`;
-  const questionUpdateId = React.createRef();
-  const questionUpdateName = React.createRef();
-  const questionUpdateParam1 = React.createRef();
-  const questionUpdateParam2 = React.createRef();
-  const questionUpdateParam3 = React.createRef();
-  const questionUpdateParam4 = React.createRef();
-  const questionUpdateParam5 = React.createRef();
-  let questionUpdateAnswerType = `BOOL`;
-
-  const handleSubmitQuestion = e => {
-    e.preventDefault();
-    questionStore.create({
-      questionValue: questionName.current.value,
-      param1: questionParam1.current.value,
-      param2: questionParam2.current.value,
-      param3: questionParam3.current.value,
-      param4: questionParam4.current.value,
-      param5: questionParam5.current.value,
-      answerType: questionAnswerType
-    });
-  };
-
-  const handleUpdateQuestion = e => {
-    e.preventDefault();
-    questionStore.update({
-      _id: questionUpdateId.current.value,
-      questionValue: questionUpdateName.current.value,
-      param1: questionUpdateParam1.current.value,
-      param2: questionUpdateParam2.current.value,
-      param3: questionUpdateParam3.current.value,
-      param4: questionUpdateParam4.current.value,
-      param5: questionUpdateParam5.current.value,
-      answerType: questionUpdateAnswerType
-    });
-  };
-
-  const handleDeleteQuestion = e => {
-    e.preventDefault();
-    questionStore.delete(e.currentTarget.children[0].value);
-  };
-
-  const checkChangeAnswerType = e => {
-    questionAnswerType = e.currentTarget.value;
-  };
-
-  const checkChangeUpdateAnswerType = e => {
-    questionUpdateAnswerType = e.currentTarget.value;
-  };
-
-  const checked = (type, answerType) => {
-    if (type === answerType) return true;
-    else return false;
-  };
-
   return (
     <>
-        <div>
-            <h2>questions</h2>
-            <form method="post">
-                <input type="text" name="clientToken" id="clientToken" value="value"/>
-                <label for="clientToken">Client Token</label>
-                <div number="3">
-                    <h3>text</h3>
-                    <div>
-                        <div>
-                            <input type="radio" name="question" id="optionsRadios1" value="true"/>
-                            <label for="optionsRadios1">
-                                Yes
-                            </label>
-                        </div>
-                        <div >
-                            <input type="radio" name="question" id="optionsRadios2" value="false"/>
-                            <label for="optionsRadios2">
-                                No
-                            </label>
-                        </div>
-                        <input type="text" name="question" id="optionsText2"/>
-                        <label for="optionsText2">
-                            Input
-                        </label>
-                    </div>
-                    <div >
-                        <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>4</div>
-                        <div>5</div>
-
-                    </div>
-                </div>
-                <div>
-                    <input type="submit" name="action" value="Submit" />
-                </div>
-            </form>
-        </div>
+      <div>
+        <h2>questions</h2>
+        <form method="post">
+          <input
+            type="text"
+            name="clientToken"
+            id="clientToken"
+            value="value"
+          />
+          <label for="clientToken">Client Token</label>
+          <div number="3">
+            <h3>text</h3>
+            <div>
+              <div>
+                <input
+                  type="radio"
+                  name="question"
+                  id="optionsRadios1"
+                  value="true"
+                />
+                <label for="optionsRadios1">Yes</label>
+              </div>
+              <div>
+                <input
+                  type="radio"
+                  name="question"
+                  id="optionsRadios2"
+                  value="false"
+                />
+                <label for="optionsRadios2">No</label>
+              </div>
+              <input type="text" name="question" id="optionsText2" />
+              <label for="optionsText2">Input</label>
+            </div>
+            <div>
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+            </div>
+          </div>
+          <div>
+            <input type="submit" name="action" value="Submit" />
+          </div>
+        </form>
+      </div>
     </>
   );
 };
