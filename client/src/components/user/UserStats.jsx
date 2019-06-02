560de2aad890aa2b9746a302f6182f8ce3e34335
Fredcyp <frederikcypers1705@gmail.com>
2019-06-02 11:42:09 +0200

    useefeect stats

--- client/src/components/user/UserStats.jsx
+++ client/src/components/user/UserStats.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { inject, observer, PropTypes } from "mobx-react";
 import Stats from "./Stats";
 import { withRouter } from "react-router-dom";
@@ -8,36 +8,37 @@ const UserStats = ({ questionStore, answerStore }) => {
   const [param3, setParam3] = useState(0);
   const [param4, setParam4] = useState(0);
   const [param5, setParam5] = useState(0);
-  let answersCount = 0;
+  const [answersCount, setAnswersCount] = useState(0);
 
   useEffect(() => {
-    answerStore.getAllByUser().then(answers => {
-      questionStore.findAll().then(questions => {
-        answers.forEach(answer => {
-          questions.forEach(question => {
-            if (question._id === answer.questionId) {
-              answersCount+=1;
-              if(answer.value){
-                setParam1(param1+90);
-                setParam2(param2+question.param2);
-                setParam3(param3+question.param3);
-                setParam4(param4+question.param4);
-                setParam5(param5+question.param5);
-                console.log(param1);
-              }
+    const f = async () => {
+      const answers = await answerStore.getAllByUser();
+      const questions = await questionStore.findAll();
+      answers.forEach(answer => {
+        questions.forEach(question => {
+          if (question._id === answer.questionId) {
+            setAnswersCount(answersCount + 1);
+            if (answer.value) {
+              setParam1(param1 + 90);
+              setParam2(param2 + question.param2);
+              setParam3(param3 + question.param3);
+              setParam4(param4 + question.param4);
+              setParam5(param5 + question.param5);
+              console.log(param1);
             }
-          });
+          }
         });
       });
-    });
-    return () => ``;
-  }, [questionStore, answerStore]);
+    };
+    f();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-      <>
-        <h2>Stats</h2>
-        <Stats params={[param1, param2, param3, param4, param5]} />
-      </>
+    <>
+      <h2>Stats</h2>
+      <Stats params={[param1, param2, param3, param4, param5]} />
+    </>
   );
 };
 
