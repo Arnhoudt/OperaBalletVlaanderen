418af0a58490706c82e4a4587e47bf34167c4344
Frederik Cypers <frederikcypers@Frederik-Cypers.local>
2019-06-09 08:08:32 +0200

    working data visualisation dashboard

--- client/src/components/admin/Answers.jsx
+++ client/src/components/admin/Answers.jsx
@@ -9,23 +9,16 @@ class Answers extends Component {
     this.answerStore = props.answerStore;
     this.colors = [`#ec5f67`, `#f99156`, `#fac863`, `#99c795`, `#5fb3b3`, `#6699cc`, `#c494c5`, `#aa7967`];
     this.state = { questions: [] };
-    this.ref0 = React.createRef();
-    this.ref1 = React.createRef();
-    this.ref2 = React.createRef();
-    this.ref3 = React.createRef();
-    this.ref4 = React.createRef();
-    this.ref5 = React.createRef();
-    this.ref6 = React.createRef();
-    this.ref7 = React.createRef();
+    this.refCharts = [];
   }
 
   componentDidMount() {
     let state = { ...this.state };
-
     this.answerStore.answers.forEach(answer => {
       if (this.state.questions.filter(question => question.question === answer.question).length === 0) {
         let q = { question: answer.question, answers: [] };
         state.questions.push(q);
+        this.refCharts.push(React.createRef());
       }
       if (this.state.questions.filter(question => question.question === answer.question)[0].answers) {
         if (this.state.questions.filter(question => question.question === answer.question)[0].answers.filter(a => a.value === answer.answer).length > 0) {
@@ -35,9 +28,10 @@ class Answers extends Component {
         }
       }
     });
-
     this.setState(state);
+  }
 
+  componentDidUpdate() {
     this.state.questions.forEach((question, index) => {
       let answersData = [];
       let answersValue = [];
@@ -67,7 +61,7 @@ class Answers extends Component {
         }
       };
 
-      const myChartRef = this[`ref${0}`].current.getContext(`2d`);
+      const myChartRef = this.refCharts[index].current.getContext(`2d`);
       new Chart(myChartRef, config);
     });
   }
@@ -75,14 +69,19 @@ class Answers extends Component {
   render() {
     return (
       <>
-        <div className={styles.containerChart}>
-          <canvas ref={this[`ref${0}`]} />
-        </div>
-        {/* {this.state.questions.map((question, index) => (
-          <div key={index} className={styles.containerChart}>
-            <canvas ref={this[`ref${index}`]} />
+        {this.state.questions.map((question, index) => (
+          <div key={index} className={styles.containerQuestion}>
+            <div>
+              <h2 className={styles.question}>{question.question}</h2>
+              <div className={styles.metadata}>
+                <p>{question.answers.length} antwoorden</p>
+              </div>
+            </div>
+            <div className={styles.containerChart}>
+              <canvas ref={this.refCharts[index]} />
+            </div>
           </div>
-        ))} */}
+        ))}
       </>
     );
   }
