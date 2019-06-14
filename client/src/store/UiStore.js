03c345e9b076be64ad0fe5a33bcba3a47d5c1c14
Frederik Cypers <frederikcypers1705@gmail.com>
2019-06-14 06:36:44 +0200

    performance

--- client/src/store/UiStore.js
+++ client/src/store/UiStore.js
@@ -3,7 +3,6 @@ import Auth from "../api/auth";
 import { getUserFromCookie, getAdminFromCookie, getRandomFromCookie } from "../utils/index.js";
 
 class UiStore {
-  authUser = null;
   authAdmin = null;
   randomUser = null;
   error = ``;
@@ -12,7 +11,6 @@ class UiStore {
     this.rootStore = rootStore;
     this.authServiceUser = new Auth(`user`);
     this.authServiceAdmin = new Auth(`admin`);
-    this.setUser(getUserFromCookie());
     this.setAdmin(getAdminFromCookie());
     if (!getUserFromCookie()) {
       if (getRandomFromCookie()) {
@@ -26,7 +24,6 @@ class UiStore {
   emptyError = () => (this.error = ``);
   changeError = error => (this.error = error);
 
-  setUser = value => (this.authUser = value);
   setAdmin = value => (this.authAdmin = value);
   setRandomUser = value => (this.randomUser = value);
 
@@ -51,50 +48,20 @@ class UiStore {
   logoutAdmin = () => {
     return this.authServiceAdmin.logout().then(data => this.setAdmin(null));
   };
-
-  loginUser = (username, password) => {
-    return this.authServiceUser.login(username, password).then(data => {
-      this.setUser(getUserFromCookie());
-      this.emptyError();
-      return data;
-    });
-  };
-
-  registerUser = (name, email, pwd) => {
-    this.emptyError();
-    return this.authServiceUser.register(name, email, pwd);
-  };
-
-  logoutUser = () => {
-    return this.authServiceUser.logout().then(data => this.setUser(null));
-  };
-
-  deleteUser = () => {
-    return this.authServiceUser.delete().then(data => {
-      this.setUser(null);
-      return data;
-    });
-  };
 }
 
 decorate(UiStore, {
-  authUser: observable,
   authAdmin: observable,
-  setUser: action,
   setAdmin: action,
   changeError: action,
   loginAdmin: action,
   registerAdmin: action,
   logoutAdmin: action,
-  loginUser: action,
-  registerUser: action,
-  logoutUser: action,
   error: observable,
   emptyError: action,
   randomUser: observable,
   setRandomUser: action,
-  registerRandom: action,
-  deleteUser: action
+  registerRandom: action
 });
 
 export default UiStore;
--- client/src/three/Canary.js
+++ client/src/three/Canary.js
@@ -1,6 +1,5 @@
 import * as THREE from "three";
 import { POINTER, PLANE_DIFFERENCE, PLANE_PERSPECTIVE_CONSTANTE } from "../constants";
-let SVGLoader = require(`three-svg-loader`);
 
 class Canary {
   createPng = (component, path, x, y, z, width, height, planeZ, anisotropy, filter, name) => {
@@ -101,21 +100,6 @@ class Canary {
     photo.object.scale.set(data.scaleX, data.scaleY, data.scaleZ);
   };
 
-  createImage = (component, path, x, y, z, width, height, name) => {
-    component.textureLoader.load(path, texture => {
-      let img = new THREE.MeshBasicMaterial({
-        map: texture
-      });
-      let plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), img);
-      plane.overdraw = true;
-      plane.position.set(x, y, z);
-      plane.scale.set(width, height, 1);
-      plane.name = name;
-
-      component.scene.add(plane);
-    });
-  };
-
   createText = (that, message, textFont, size, color, x, y, z, planeZ, rotation = 0, name, center) => {
     that.fontLoader.load(textFont, font => {
       let textGeometry = new THREE.TextGeometry(message, {
@@ -141,52 +125,6 @@ class Canary {
     });
   };
 
-  createHollowText = (component, textFont, textColor, textMessage, textPosX, textPosY, textPosZ, textSize) => {
-    component.fontLoader.load(textFont, font => {
-      let xMid;
-      let color = new THREE.Color(textColor);
-      let matDark = new THREE.MeshBasicMaterial({
-        color: color,
-        side: THREE.DoubleSide
-      });
-      // let matLite = new THREE.MeshBasicMaterial({
-      //   color: color,
-      //   transparent: true,
-      //   opacity: 0.4,
-      //   side: THREE.DoubleSide
-      // });
-      let shapes = font.generateShapes(textMessage, textSize);
-      let geometry = new THREE.ShapeBufferGeometry(shapes);
-      geometry.computeBoundingBox();
-      xMid = -0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x);
-      geometry.translate(xMid, 0, 0);
-      // make line shape ( N.B. edge view remains visible )
-      let holeShapes = [];
-      for (let i = 0; i < shapes.length; i++) {
-        let shape = shapes[i];
-        if (shape.holes && shape.holes.length > 0) {
-          for (let j = 0; j < shape.holes.length; j++) {
-            let hole = shape.holes[j];
-            holeShapes.push(hole);
-          }
-        }
-      }
-      shapes.push.apply(shapes, holeShapes);
-      let style = SVGLoader.getStrokeStyle(5, color.getStyle());
-      let strokeText = new THREE.Group();
-      for (let i = 0; i < shapes.length; i++) {
-        let shape = shapes[i];
-        let points = shape.getPoints();
-        let geometry2 = SVGLoader.pointsToStroke(points, style);
-        geometry2.translate(xMid, 0, 0);
-        let strokeMesh = new THREE.Mesh(geometry2, matDark);
-        strokeText.add(strokeMesh);
-      }
-      strokeText.position.set(textPosX, textPosY - textSize / 2, textPosZ);
-      component.scene.add(strokeText);
-    }); //end load function
-  };
-
   createPointer = path => {
     let pointer = document.createElement(`div`);
     pointer.innerHTML = `<img src=` + path + ` width="` + POINTER.width + `" height="` + POINTER.height + `" alt="">`;
@@ -201,7 +139,7 @@ class Canary {
     let scroll = document.createElement(`div`);
     scroll.classList.add(`icon-scroll`);
     return scroll;
-  }
+  };
 
   changePointer = (pointer, path) => {
     pointer.innerHTML = `<img src=` + path + ` width="` + POINTER.width + `" height="` + POINTER.height + `" alt="">`;
