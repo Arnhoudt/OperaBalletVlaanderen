d8fa8859f7bc50b6d24845f8d1751ae010890508
Anton Kindt <anton.kindt@ugent.be>
2022-03-10 19:54:32 +0100

    changed things

---
+++ client/build/precache-manifest.dec549ac26c15c27ed0a3b67261dec82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15c2340889944031ef9dc39fa1b607ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "351fc6a520e683e25763",
+    "url": "/static/css/main.4e7cf192.chunk.css"
+  },
+  {
+    "revision": "a48a40da5681cb855a31",
+    "url": "/static/js/2.ac26e415.chunk.js"
+  },
+  {
+    "revision": "351fc6a520e683e25763",
+    "url": "/static/js/main.9bbac225.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
